2ec20a8513b66addaf1a14e6de9f088dcba44c55
Yan Cao <yc@example.com>
2024-06-29 06:23:25 +0800

    flake
    Signed-off-by: Yan Cao <yc@example.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "NixOS configuration with flakes";
-  inputs.nixpkgs.url = "nixpkgs/release-24.05";
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  # https://hydra.nixos.org/job/nixos/release-24.05/tested#tabs-status
+  inputs.nixpkgs.url = "nixpkgs/e4509b3a560c87a8d4cb6f9992b8915abf9e36d8";
+  # https://github.com/nixos/nixos-hardware
+  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/3980e7816c99d9e4da7a7b762e5b294055b73b2f";
 
   outputs = { self, nixpkgs, nixos-hardware }: {
     nixosConfigurations.yinzhou = nixpkgs.lib.nixosSystem {
