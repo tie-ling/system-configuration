0fcedc68f4050db623dc1dfd163bf11915bcef11
Yan Cao <yc@example.com>
2024-07-24 17:21:43 +0800

    server: add openssh key for our

--- server/configuration.nix
+++ server/configuration.nix
@@ -88,6 +88,7 @@
       our = {
         isNormalUser = true;
         openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdVFa8xiHlDR9keRNERhNysEfdLrk/oKOFc+U8bQFAE u0_a298@localhost"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWeAeIuIf2Zyv+d+J6ZWGuKx1lmKFa6UtzCTNtB5+Ev openpgp:0x1FD7B98A"
         ];
       };
