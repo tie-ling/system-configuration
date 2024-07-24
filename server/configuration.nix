e785e1c7994292c6e8356a1e69296329d5eb2871
Yan Cao <yc@example.com>
2024-07-24 19:21:43 +0800

    upgrade transmission

--- server/configuration.nix
+++ server/configuration.nix
@@ -177,6 +177,7 @@
       };
       transmission = {
         enable = true;
+        package = pkgs.transmission_4
         home = "/rtorrent";
         downloadDirPermissions = "755";
         openFirewall = true;
