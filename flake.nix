4743aa26726a045a3597f0376f3606ce984fb8d5
Your Name <you@example.com>
2024-10-30 21:12:15 +0100

    update nixos version

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
   description = "NixOS configuration with flakes";
   # https://hydra.nixos.org/job/nixos/release-24.05/tested#tabs-status
-  inputs.nixpkgs.url = "nixpkgs/f1bad50880bae73ff2d82fafc22010b4fc097a9c";
+  inputs.nixpkgs.url = "nixpkgs/cd3e8833d70618c4eea8df06f95b364b016d4950";
   # https://github.com/nixos/nixos-hardware
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/3980e7816c99d9e4da7a7b762e5b294055b73b2f";
   outputs =
     {
       self,
       nixpkgs,
-      nixos-hardware,
     }@inputs:
     {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
@@ -19,8 +17,6 @@
         modules = [
           ./laptop/configuration.nix
           ./laptop/hardware-configuration.nix
-          # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-          nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen4
         ];
       };
       nixosConfigurations.tieling = nixpkgs.lib.nixosSystem {
