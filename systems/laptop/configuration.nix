dd9aa546742147b5166cacc09b7f0c8f04b817f5
Yan Cao <yc@example.com>
2025-01-04 09:31:41 +0100

    laptops: grub for non-efi computers

--- systems/laptop/configuration.nix
+++ systems/laptop/configuration.nix
@@ -74,6 +74,9 @@ in
     "sdhci_pci"
   ];
   system.stateVersion = "24.05"; # Did you read the comment?
+  # for non-efi laptops
+  boot.loader.grub.enable = true;
+
 
   # security; remember to enable hardened mode
   security.lockKernelModules = false;
