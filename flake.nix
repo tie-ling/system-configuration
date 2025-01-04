3efee1777f1a3998bba3859e9ef6c4d8f4e04370
Yan Cao <yc@example.com>
2025-01-04 08:58:33 +0100

    disko: merge configurations for laptop
    disko: merge

--- flake.nix
+++ flake.nix
@@ -27,6 +27,7 @@
                 modules = [
                   disko.nixosModules.disko
                   ./systems/laptop/configuration.nix
+                  ./systems/laptop/disko/configuration.nix
                   ./systems/laptop/disko/${laptopName}.nix
                 ];
               }
--- systems/laptop/disko/dell-7370.nix
+++ systems/laptop/disko/dell-7370.nix
@@ -1,85 +1,8 @@
 {
-  # see https://github.com/nix-community/disko/tree/master/example
-  # in particular
-  # https://github.com/nix-community/disko/blob/master/example/zfs-with-vdevs.nix
-  # https://github.com/nix-community/disko/blob/master/example/zfs.nix
   disko.devices = {
     disk = {
       main = {
-        type = "disk";
         device = "/dev/disk/by-id/ata-INTEL_SSDSCKKF256G8H_BTLA81651HQR256J";
-        content = {
-          type = "gpt";
-          partitions = {
-            # gpt-bios-compat
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
-            ESP = {
-              size = "1024M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            encryptedSwap = {
-              size = "8G";
-              content = {
-                type = "swap";
-                randomEncryption = true;
-              };
-            };
-            luks-root = {
-              # contains swap file
-              size = "64G";
-              content = {
-                type = "luks";
-                name = "crypted-root";
-                settings.allowDiscards = true;
-                passwordFile = "/tmp/secret.key";
-                content = {
-                  type = "filesystem";
-                  format = "xfs";
-                  mountpoint = "/";
-                };
-              };
-            };
-            luks-home = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "crypted-home";
-                settings.allowDiscards = true;
-                passwordFile = "/tmp/secret.key";
-                content = {
-                  type = "zfs";
-                  pool = "zpool-home";
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-    zpool = {
-      zpool-home = {
-        type = "zpool";
-        # mode = one of "", "mirror", "raidz{1,2,3}"
-        # see definition in https://github.com/nix-community/disko/blob/master/lib/types/zpool.nix
-        mode = "";
-        mountpoint = "/zpool-home";
-        options.compatibility = "legacy";
-        datasets = {
-          home = {
-            type = "zfs_fs";
-            mountpoint = "/home";
-            options."com.sun:auto-snapshot" = "true";
-          };
-        };
       };
     };
   };
